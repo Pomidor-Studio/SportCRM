986cc822ba39217ce8f526fa7fa26d21b7e64ae9
vlasov1994 <Kron1994>
2019-01-17 01:31:21 +0500

    Добавил навигационное меню (Navbar)

--- crm/urls.py
+++ crm/urls.py
@@ -14,7 +14,7 @@ from . import views
 
 app_name = 'crm'
 urlpatterns = [
-    path('', views.home, name='crm-home'),
+    path('', views.home, name='home'),
     path('clients/', views.ClientsListView.as_view(), name='clients'),
     path('clients/<int:pk>/', views.ClientDetailView.as_view(), name='client-detail'),
     path('eventclass/', views.EventClassList.as_view(), name='eventclass_list'),
@@ -22,21 +22,11 @@ urlpatterns = [
     path('eventclass/<int:pk>/update/', views.EventClassUpdate.as_view(), name='eventclass_update'),
     path('eventclass/<int:pk>/delete/', views.EventClassDelete.as_view(), name='eventclass_delete'),
     path('clients/new/', ClientCreateView.as_view(), name='client-new'),
-    path('clients/<int:pk>/update/',
-         ClientUpdateView.as_view(), name='client-update'),
-    path('clients/<int:pk>/delete/',
-         ClientDeleteView.as_view(), name='client-delete'),
-
-    path('subscriptions/', SubscriptionsListView.as_view(),
-         name='subscriptions'),
-    path('subscriptions/<int:pk>/update/',
-         SubscriptionUpdateView.as_view(), name='subscription-update'),
-    path('subscriptions/<int:pk>/delete/',
-         SubscriptionDeleteView.as_view(), name='subscription-delete'),
-    path('subscriptions/new/', SubscriptionCreateView.as_view(),
-         name='subscription-new'),
-
-    path(r'^add-client-subscription/(?P<client>\d+)/$', ClientSubscriptionCreateView.as_view(),
-         name='clientsubscription-new'),
-
+    path('clients/<int:pk>/update/', ClientUpdateView.as_view(), name='client-update'),
+    path('clients/<int:pk>/delete/', ClientDeleteView.as_view(), name='client-delete'),
+    path('subscriptions/', SubscriptionsListView.as_view(), name='subscriptions'),
+    path('subscriptions/<int:pk>/update/', SubscriptionUpdateView.as_view(), name='subscription-update'),
+    path('subscriptions/<int:pk>/delete/', SubscriptionDeleteView.as_view(), name='subscription-delete'),
+    path('subscriptions/new/', SubscriptionCreateView.as_view(), name='subscription-new'),
+    path(r'^add-client-subscription/(?P<client>\d+)/$', ClientSubscriptionCreateView.as_view(), name='clientsubscription-new'),
 ]
--- sportcrm/urls.py
+++ sportcrm/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include
+from django.urls import path
 
+app_name = 'sportcrm'
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
     path('', include('crm.urls')),
 ]
