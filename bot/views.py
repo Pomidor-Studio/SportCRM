e097e031e70ede0758427de458c625791d72dbda
Ilya Loginov <ilya2707004@yandex.ru>
2019-01-19 01:24:21 +0500

    Тест

--- bot/views.py
+++ bot/views.py
@@ -5,8 +5,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from bot.api.settings import *
 from bot.api.messageHandler import create_answer
+from bot.api.vkapi import send_message
 
-import json, vk # vk is library from VK
+import json, vk
 
 """
 Using VK Callback API version 5.5
@@ -28,9 +29,13 @@ For cross site request protection will be used secret key from VK
 def gl(request): #url: https://mysite.ru/vkbot/
     if (request.method == "POST"):
         data = json.loads(request.body)# take POST request from auto-generated variable <request.body> in json format
-        if (data['type'] == 'confirmation'):# if VK server request confirmation
-            # confirmation_token from settings.py
-            return HttpResponse(confirmation_token, content_type="text/plain", status=200)
-        if (data['type'] == 'message_new'):# if VK server send a message
-            create_answer(data['object'], token)
-            return HttpResponse('ok', content_type="text/plain", status=200)
+        if (data['secret'] == secret_key):# if json request contain secret key and it's equal my secret key
+            if (data['type'] == 'confirmation'):# if VK server request confirmation
+
+                # confirmation_token from settings.py
+                return HttpResponse(confirmation_token, content_type="text/plain", status=200)
+            if (data['type'] == 'message_new'):# if VK server send a message
+                user_id = data['object']['user_id']
+                send_message(token, str(user_id), "Hello, I'm bot!")
+                #create_answer(data['object'], token)
+                return HttpResponse('ok', content_type="text/plain", status=200)
