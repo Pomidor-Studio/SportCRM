7177c37daaf031992a4f9d67da61cc76064c51fd
Ilya Loginov <ilya2707004@yandex.ru>
2019-01-20 01:22:07 +0500

    Проверка работоспособности

--- bot/api/vkapi.py
+++ bot/api/vkapi.py
@@ -7,4 +7,3 @@ def send_message(user_id, token, message, attachment=""):
     api = vk.API(session, v=5.5)
 
     api.messages.send(access_token=token, user_id=str(user_id), message=message, attachment=attachment)
-    return HttpResponse('ok', content_type="text/plain", status=200)
