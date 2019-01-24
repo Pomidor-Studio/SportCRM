b745ee2ce1413222a0878b777a80bda0c7ae17a9
Ilya Loginov <ilya2707004@yandex.ru>
2019-01-24 18:44:24 +0500

    Тестовая отправка

---
+++ bot/api/commands/clients.py
@@ -0,0 +1,13 @@
+from crm.models import SubscriptionsType
+from bot.api.command_system import Command
+
+def test():
+    a = SubscriptionsType.objects.all()
+    message = a + '\n gg'
+    return message, ''
+
+test_command = Command()
+
+test_command.keys = ['test']
+test_command.description = 'test'
+test_command.process = test
