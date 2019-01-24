cdb8eebc05da363e1267e08c51503e519f5fb362
Ilya Loginov <ilya2707004@yandex.ru>
2019-01-24 18:41:22 +0500

    Убрал лишний код

--- bot/views.py
+++ bot/views.py
@@ -5,7 +5,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from bot.api.settings import *
 from bot.api.messageHandler import create_answer
-from bot.api.vkapi import send_message
 
 import json, vk
 
@@ -32,12 +31,8 @@ def gl(request): #url: https://mysite.ru/vkbot/
         if (data['secret'] == secret_key):# if json request contain secret key and it's equal my secret key
             if (data['type'] == 'confirmation'):# if VK server request confirmation
 
-                # confirmation_token from settings.py
                 return HttpResponse(confirmation_token, content_type="text/plain", status=200)
             if (data['type'] == 'message_new'):# if VK server send a message
-                #session = vk.Session()
-                #api = vk.API(session, v=5.5)
-                #user_id = data['object']['user_id']
-                #api.messages.send(access_token=token, user_id=str(user_id), message="Hello, I'm bot!")
+
                 create_answer(data['object'], token)
                 return HttpResponse('ok', content_type="text/plain", status=200)
