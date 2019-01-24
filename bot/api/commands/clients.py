12f53087cf7648214ace3a87bfe97b296db26bc2
Ilya Loginov <ilya2707004@yandex.ru>
2019-01-24 18:56:27 +0500

    Тестовая отправка

--- bot/api/commands/clients.py
+++ bot/api/commands/clients.py
@@ -1,8 +1,8 @@
-from crm.models import SubscriptionsType
+from crm.models import Client
 from bot.api.command_system import Command
 
 def test():
-    a = SubscriptionsType.objects.all()
+    a = Client.objects.filter(id = 1)
     message = a
     return message, ''
 
