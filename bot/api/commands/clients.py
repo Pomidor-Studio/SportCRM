db13eb5fabce67a70b4e17520ea6e4742cce5d1f
Ilya Loginov <ilya2707004@yandex.ru>
2019-01-24 20:11:02 +0500

    Тестовая отправка

--- bot/api/commands/clients.py
+++ bot/api/commands/clients.py
@@ -1,18 +1,29 @@
-from crm.models import SubscriptionsType
+from crm.models import SubscriptionsType, Client, ClientSubscriptions
 from bot.api.command_system import Command
 
+id_client = 1
+
 def test():
-    a = SubscriptionsType.objects.filter(id = 1)
+    a = SubscriptionsType.objects.filter(id = id_client)
+    b = Client.objects.filter(id = id_client)
+    c = ClientSubscriptions.objects.filter(client_id = id_client)
     k = {}
+    kk = {}
     i = 0
-    for sub in a:
-        c = sub.name
-        b = sub.price
-        d = sub.duration
-        j = sub.visit_limit
-        k[i] = [c, b, d, j]
+    for cl in b:
+        name = cl.name
+        k[i] = [name]
         i += 1
-    message = str(k[0])
+    for sub in a:
+        name = sub.name
+        k[i] = [name]
+        i += 1
+    for sub in c:
+        purchase_date = sub.purchase_date
+        k[i] = [purchase_date]
+        i += 1
+    kk[0] = [k]
+    message = str(kk[0])
     return message, ''
 
 test_command = Command()
