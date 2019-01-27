3ea3112dfcf16cf0b82dc6bcfcb721a0b0fe1322
Ilya Loginov <ilya2707004@yandex.ru>
2019-01-27 15:00:28 +0500

    Написал функцию поиска абонементов по текущему user_id

--- bot/api/commands/clients.py
+++ bot/api/commands/clients.py
@@ -1,34 +1,47 @@
 from crm.models import SubscriptionsType, Client, ClientSubscriptions
 from bot.api.command_system import Command
+from bot.api.messageHandler import user_id1 #Обновить репозиторий, сделать по ВК_ИД, привести в нормальный вид, Список отправлять в нормальном виде
 
-id_client = 2
-id_sub = 1
+vk_user_id = user_id1
 
-def test():
-    a = SubscriptionsType.objects.filter(id = id_sub)
-    b = Client.objects.filter(id = id_client)
-    c = ClientSubscriptions.objects.filter(client_id = id_client)
+def get_info_abonements():
+    client = Client.objects.filter(vk_user_id=vk_user_id)
+
     k = {}
-    kk = {}
+    sub_list = ''
     i = 0
-    for cl in b:
+
+    for cl in client:
+        id = cl.id
         name = cl.name
-        k[i] = [name]
+
+    subscription = ClientSubscriptions.objects.filter(client_id=id)
+
+    for sub in subscription:
+        subscription = sub.subscription
+        visits_left = sub.visits_left
+        k[i] = [subscription, visits_left]
         i += 1
-    for sub in a:
-        name = sub.name
-        k[i] = [name]
-        i += 1
-    for sub in c:
-        purchase_date = sub.purchase_date
-        k[i] = [purchase_date]
-        i += 1
-    kk[0] = [k]
-    message = str(kk[0])
+
+    sub = str(k)
+    i = 0
+
+    for sub in sub:
+        if sub == '{' or sub == '[' or sub == '}':
+            continue
+        if sub == ']':
+            i = 1
+            continue
+        if i == 1:
+            sub_list = sub_list + '\n'
+            continue
+        sub_list = sub_list + sub
+
+    message = str(name + '!\n Информация о ваших абонементах:' + '\n' + sub_list)
     return message, ''
 
 test_command = Command()
 
-test_command.keys = ['test']
-test_command.description = 'test'
-test_command.process = test
+test_command.keys = ['информация', 'абонементы', 'мои абонементы', 'информация о моих абонементах']
+test_command.description = 'Информация о Ваших абонементах'
+test_command.process = get_info_abonements
