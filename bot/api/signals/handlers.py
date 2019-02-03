b9ce9dfe06279de8f809dad7cc5bf86e13e28dcc
Ilya Loginov <ilya2707004@yandex.ru>
2019-02-04 00:02:13 +0500

    Реализация уведомлений при добавлении записи в БД, при остатке 1 посещения на абонементе

--- bot/api/signals/handlers.py
+++ bot/api/signals/handlers.py
@@ -1,4 +1,3 @@
-#Поправить парсер!
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from crm.models import ClientSubscriptions
@@ -7,13 +6,28 @@ from bot.api.vkapi import send_message
 
 
 @receiver(post_save, sender=ClientSubscriptions)
-def sub_client(sender, **kwargs):
+def sub_client(sender, created, **kwargs):
     client_subscription:ClientSubscriptions = kwargs['instance']
 
-    end_date = str(client_subscription.end_date)
-    end_date = end_date[0: -15]
+    vk_user_id = client_subscription.client.vk_user_id
 
-    message = client_subscription.client.name +'!\nВы приобрели абонемент:\n ' + client_subscription.subscription.name + \
-              '!\nДействующий до:\n ' + str(end_date)
+    if vk_user_id == None:
+        return
 
-    send_message(client_subscription.client.vk_user_id, token, message, '')
+    if created == 1:
+        end_date = '{:%d-%m-%Y}'.format(client_subscription.end_date)
+
+        message = client_subscription.client.name +'!\nВы приобрели абонемент:\n ' + client_subscription.subscription.name + \
+                '!\nДействующий до:\n' + end_date
+
+        send_message(vk_user_id, token, message, '')
+
+    else:
+        visits_left = client_subscription.visits_left
+
+        if visits_left == 1:
+            message = client_subscription.client.name + '!\nНа вашем абонементе:\n' + client_subscription.subscription.name + \
+                '\nОсталось 1 посещение!'
+
+            send_message(vk_user_id, token, message, '')
+        return
