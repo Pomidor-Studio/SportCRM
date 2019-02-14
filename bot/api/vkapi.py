dad33ae6ae19eddc848c948fc5a814d1761ab25e
Ilya Loginov <ilya2707004@yandex.ru>
2019-02-14 20:04:07 +0500

    Обработал ошибку, если клиент НЕ писал боту

--- bot/api/vkapi.py
+++ bot/api/vkapi.py
@@ -6,4 +6,7 @@ def send_message(user_id, token, message, attachment=""):
     session = vk.Session()
     api = vk.API(session, v=5.5)
 
-    api.messages.send(access_token=token, user_id=str(user_id), message=message, attachment=attachment)
+    try:
+        api.messages.send(access_token=token, user_id=str(user_id), message=message, attachment=attachment)
+    except vk.exceptions.VkAPIError:
+        return
