dd04ea8a9b885a4f448a5015cf55a058ee049432
Ilya Loginov <ilya2707004@yandex.ru>
2019-02-23 15:25:31 +0500

    Настройки VK.API, сменил версию API на 5.90

--- bot/api/vkapi.py
+++ bot/api/vkapi.py
@@ -2,9 +2,9 @@ from django.http import HttpResponse
 import vk
 
 
-def send_message(user_id, token, message, attachment=""):
+def send_message(user_id, token, message, attachment=''):
     session = vk.Session()
-    api = vk.API(session, v=5.5)
+    api = vk.API(session, v=5.90)
 
     try:
         api.messages.send(access_token=token, user_id=str(user_id), message=message, attachment=attachment)
