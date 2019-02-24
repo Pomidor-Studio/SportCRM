23fee4d11a6d7139b98a6a34443bf4255f6d1dee
Ilya Loginov <ilya2707004@yandex.ru>
2019-02-24 21:45:35 +0500

    Поправил message

--- bot/api/client_receivables.py
+++ bot/api/client_receivables.py
@@ -2,14 +2,16 @@ from crm.models import Client
 from bot.api.settings import *
 from bot.api.vkapi import send_message
 
+
 def receivables():
 
     client = Client.objects.filter(balance__lt=0)
+    message = []
 
     for cl in client:
         name = cl.name
         balance = cl.balance
-        message = name + '!\nУ вас есть задолженность!\nСостояние вашего счета: ' + str(balance)
-        send_message(token, cl.vk_user_id, message, '')
+        message.extend([name, '!\nУ вас есть задолженность!\nСостояние вашего счета: ', str(balance)])
+        send_message(token, cl.vk_user_id, ''.join(message), '')
 
     return
