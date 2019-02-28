e3e840a44ac15c0c8b1dc64ff4f51938ab896351
Бояршинов <roman.boyarshinow@gmail.com>
2019-02-28 21:01:26 +0500

    Added views for locations

--- crm/views/manager/location.py
+++ crm/views/manager/location.py
@@ -1,17 +1,22 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.checks import messages
+from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django_filters.views import FilterView
 from reversion.views import RevisionMixin
 
+from crm.filters import LocationFilter
 from crm.models import Location
-from crm.views.mixin import UserManagerMixin
+from crm.views.mixin import UserManagerMixin, UnDeleteView
 
 
-class ObjList(LoginRequiredMixin, UserManagerMixin, ListView):
+class ObjList(LoginRequiredMixin, UserManagerMixin, FilterView):
     model = Location
     template_name = 'crm/manager/location/list.html'
     context_object_name = 'locations'
     ordering = ['id']
+    filterset_class = LocationFilter
 
 
 class Create(LoginRequiredMixin, UserManagerMixin, RevisionMixin, CreateView):
@@ -30,3 +35,24 @@ class Delete(LoginRequiredMixin, UserManagerMixin, RevisionMixin, DeleteView):
     model = Location
     success_url = reverse_lazy('crm:manager:locations:list')
     template_name = 'crm/manager/location/confirm_delete.html'
+
+class Undelete(
+    LoginRequiredMixin,
+    UserManagerMixin,
+    RevisionMixin,
+    UnDeleteView
+):
+    template_name = 'crm/manager/coach/confirm_undelete.html'
+    model = Location
+    context_object_name = 'location'
+    success_url = reverse_lazy('crm:manager:location:list')
+
+    def undelete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        user = self.object.user
+        success_url = self.get_success_url()
+        self.object.undelete()
+        user.is_active = True
+        user.save()
+        messages.info(self.request, f'Место {self.object} возвращено.')
+        return HttpResponseRedirect(success_url)
