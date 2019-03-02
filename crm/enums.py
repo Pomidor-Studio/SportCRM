41df602b38d8eff7313958c1359cf420ad3a0d98
Kirill Malovitsa <ciklop.kirill@gmail.com>
2019-03-02 11:03:23 +0500

    Cancel one event (#25)
    * Add helper method  get next event class date
    
    * Test for all edge cases
    
    * Rewrite get_calendar to generator
    
    * Add filter range for calendar
    
    * Doc update
    
    * Add implicit order
    
    * Add cancellation fields
    
    * Add test for client subscription nearest event after end date
    
    * Fix documentation
    
    * Add tests to extension cases
    
    * Fix merge error
    
    * ClientSubscription dates are changed to tz-less dates
    
    * SuscriptionsType fix start/end date
    
    Added django-extended-choices
    Fixed subscriptionstype end_date calculation
    
    * Add tests for all cases of event cancellation or extending
    
    * Add cancel event actions
    
    Add more rules for event management
    Add utility class for "act-as-ajax-actions"
    
    * Fixed expiring datetime error
    
    * Add event undo cancellation
    
    Only activating without revoking is done
    For revoking extensions only interface is ready

---
+++ crm/enums.py
@@ -0,0 +1,8 @@
+from extended_choices import Choices
+
+GRANULARITY = Choices(
+    ('DAY', 'day', 'День'),
+    ('WEEK', 'week', 'Неделя'),
+    ('MONTH', 'month', 'Месяц'),
+    ('YEAR', 'year', 'Год')
+)
