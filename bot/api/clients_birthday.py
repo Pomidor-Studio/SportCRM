df4868856d49838672e13990edef677326e0c95e
Ilya Loginov <ilya2707004@yandex.ru>
2019-03-02 20:06:25 +0500

    Уведомление о Дне рождения ученика ученику в ВК Уведомление о Дне рождения менеджеру в ВК

--- bot/api/clients_birthday.py
+++ bot/api/clients_birthday.py
@@ -9,26 +9,26 @@ def birthday():
     month = current_date.month
     day = current_date.day
 
-    company = Company.objects.filter()
+    companies = Company.objects.filter()
 
-    for comp in company:
-        message_manage = []
-        message_manage.append('Сегодня День рождение:\n')
+    for company in companies:
+        message_managers = []
+        message_managers.append('Сегодня День рождение:\n')
 
         clients = Client.objects.filter(birthday__month=month,
                                         birthday__day=day,
-                                        company_id=comp.id)
+                                        company_id=company.id)
 
         for cl in clients:
             message = []
-            message_manage.extend([cl.name, '(vk.com/id', str(cl.vk_user_id), ')\n'])
+            message_managers.extend([cl.name, '(vk.com/id', str(cl.vk_user_id), ')\n'])
             message.extend([cl.name, '!\n'])
             message.append('Поздравляем вас с Днем рождения!')
             send_message(cl.vk_user_id, token, ''.join(message), '')
 
-        manage = Manager.objects.filter(company_id=comp.id)
+        managers = Manager.objects.filter(company_id=company.id)
 
-        for manage in manage:
-            send_message(manage.user.vk_id, token, ''.join(message_manage), '')
+        for manager in managers:
+            send_message(manager.user.vk_id, token, ''.join(message_managers), '')
 
     return
