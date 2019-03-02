6517fcafa6223fbc560dfef44d8c5a6f61559f27
Бояршинов <roman.boyarshinow@gmail.com>
2019-03-02 20:23:17 +0500

    Подключение библиотек

--- sportcrm/settings/base.py
+++ sportcrm/settings/base.py
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_select2',
+    'django_tables2',
 ]
 
 BOOTSTRAP4 = {
