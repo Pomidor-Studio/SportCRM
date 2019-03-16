176039d9d934be1e36ee743a709c480e3ced2e1e
Ilya Loginov <ilya2707004@yandex.ru>
2019-03-16 17:44:16 +0500

    Добавил классы для уведомления по Tasks

--- bot/api/messages/event.py
+++ bot/api/messages/event.py
@@ -1,5 +1,13 @@
-from bot.api.messages.base import Message
+from bot.api.messages.base import Message, MessageManager
 from crm.models import Event
+
+
+class ManagerEventMessage(MessageManager):
+
+    def __init__(self, manager, personalized=True, *, event: Event):
+        self.event = event
+
+        super().__init__(manager, personalized)
 
 
 class CancelledEvent(Message):
@@ -13,4 +21,24 @@ class CancelledEvent(Message):
         return (
             f'Была отменена тренировка на {self.event.date} '
             f'по {self.event.event_class}'
+        )
+
+
+class ClosedEvent(ManagerEventMessage):
+
+    def prepare_generalized_msg(self):
+        return (
+            f'Была закрыта тренировка на {self.event.date} '
+            f'по {self.event.event_class}\n'
+            f'Тренером: {self.event.event_class.coach.user.first_name} {self.event.event_class.coach.user.last_name}'
+        )
+
+
+class OpenedEvent(ManagerEventMessage):
+
+    def prepare_generalized_msg(self):
+        return (
+            f'Была открыта тренировка на {self.event.date} '
+            f'по {self.event.event_class}\n'
+            f'Тренером: {self.event.event_class.coach.user.first_name} {self.event.event_class.coach.user.last_name}'
         )
