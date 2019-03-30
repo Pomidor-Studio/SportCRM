f72c9a1435032334241c10d92a50dbd96e4279a9
ILYAPC\Илья <protos.perm@gmail.com>
2019-03-30 19:44:29 +0500

    url correction

--- gcp/background_urls.py
+++ gcp/background_urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+from django.urls import path
 from django.urls import include
 from . import views
 
 urlpatterns = [
-    url('google_task_handler/', views.google_task_handler, name='google-task-handler'),
-    include('gcp.urls')
+    path('google_task_handler/', views.google_task_handler, name='google-task-handler'),
+    path('', include('gcp.urls')),
 ]
 
 
--- gcp/urls.py
+++ gcp/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
-     url('_ah/warmup', views.warm_up, name='warmup'),
+     path('_ah/warmup', views.warm_up, name='warmup'),
 ]
