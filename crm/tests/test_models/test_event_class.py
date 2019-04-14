8faea86afa5f9f5705981672e0df2596531b56e6
Kirill Malovitsa <ciklop.kirill@gmail.com>
2019-04-15 01:55:42 +0500

    Generate calendar ignoring events without empty days of week
    This case will be edge case, as event class can't anymore have
    empty week days.

--- crm/tests/test_models/test_event_class.py
+++ crm/tests/test_models/test_event_class.py
@@ -16,8 +16,7 @@ def test_nearest_infinite_no_days(event_class_factory):
         date_from=monday, days=[])
 
     assert_that(
-        calling(ec.days),
-        raises(ValueError, "Event class don't have any days to spread")
+        ec.days(), is_([])
     )
 
 
