6519b465a7168c56876f3d77bfd1a2887368c477
Kirill Malovitsa <ciklop.kirill@gmail.com>
2019-04-19 00:41:14 +0500

    Display subscription days range

--- crm/urls_api.py
+++ crm/urls_api.py
@@ -2,7 +2,9 @@ from django.urls import path, include
 
 from crm.views.manager.event_class import ApiCalendar
 from crm.views.manager import event as manager_event_views
+from crm.views.manager import subscription as manager_subscription_views
 from crm.views import coach as coach_views
+
 
 manager_event_class_urls = ([
     path('<int:pk>/calendar/', ApiCalendar.as_view(), name='calendar'),
@@ -12,9 +14,18 @@ manager_event_urls = ([
     path('', manager_event_views.ApiCalendar.as_view(), name='calendar'),
 ], 'event')
 
+manager_subscription_urls = ([
+    path(
+        '<int:pk>/sell-range/',
+        manager_subscription_views.SellRange.as_view(),
+        name='sell-range'
+    )
+], 'subscription')
+
 manager_api_urls = ([
     path('event-class/', include(manager_event_class_urls)),
     path('event/', include(manager_event_urls)),
+    path('subscription/', include(manager_subscription_urls)),
 ], 'manager')
 
 coach_api_urls = ([
