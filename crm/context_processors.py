455c0d3135a07ecd029fe099382ba4b73e505726
Kirill Malovitsa <ciklop.kirill@gmail.com>
2019-05-28 12:09:13 +0500

    Get data for users by vk linkt

--- crm/context_processors.py
+++ crm/context_processors.py
@@ -1,7 +1,14 @@
+from django.conf import settings
 from django_multitenant.utils import get_current_tenant
 
 
 def company(request):
     return {
         'company': get_current_tenant(),
+    }
+
+
+def vk_site_access_token(request):
+    return {
+        'vk_site_access_token': settings.VK_GROUP_TOKEN
     }
